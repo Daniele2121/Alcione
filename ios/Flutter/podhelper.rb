eb83f93422cdd68e483da6195a73c2478cb8637b
Danielepopo <daniballo04@gmail.com>
2026-03-28 16:39:19 +0100

    Sincronizzazione completa plugin e podhelper

--- ios/Flutter/podhelper.rb
+++ ios/Flutter/podhelper.rb
@@ -1,18 +1,21 @@
-# Questo file è generato da Flutter per collegare i plugin iOS
 def flutter_install_all_ios_pods(ios_application_path = nil)
   flutter_application_path = ios_application_path || File.join('..', '..')
+  app_config = JSON.parse(File.read(File.join(flutter_application_path, '.metadata')))
 
-  # Carica le impostazioni dei plugin
-  native_add_path = File.join(flutter_application_path, '.symlinks', 'plugins')
-  if File.exist?(native_add_path)
-    # Codemagic userà questa logica per trovare Firestore e gli altri
+  # Trova i plugin registrati nel file .flutter-plugins-dependencies
+  plugins_file = File.join(flutter_application_path, '.flutter-plugins-dependencies')
+  if File.exist?(plugins_file)
+    plugins_config = JSON.parse(File.read(plugins_file))
+    plugins_config['plugins']['ios'].each do |plugin|
+      pod plugin['name'], :path => File.join(plugin['path'], 'ios')
+    end
   end
 end
 
 def flutter_additional_ios_build_settings(target)
   return unless target.respond_to?(:build_configurations)
   target.build_configurations.each do |config|
-    # Debug e Release settings
     config.build_settings['ENABLE_BITCODE'] = 'NO'
+    config.build_settings['IPHONEOS_DEPLOYMENT_TARGET'] = '15.0'
   end
 end
