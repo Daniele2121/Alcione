4c0c19b765fd242f9b8d1fc248a23d52fc0a28c2
Danielepopo <daniballo04@gmail.com>
2026-03-28 23:52:11 +0100

    Fix: Added Bridging Header to resolve cloud_firestore module not found

--- ios/Runner/Runner-Bridging-Header.h
+++ ios/Runner/Runner-Bridging-Header.h
@@ -1,1 +1,4 @@
 #import "GeneratedPluginRegistrant.h"
+#import <cloud_firestore/FLTFirebaseFirestorePlugin.h>
+#import <firebase_auth/FLTFirebaseAuthPlugin.h>
+#import <firebase_core/FLTFirebaseCorePlugin.h>
