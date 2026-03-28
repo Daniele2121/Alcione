10286f23620842c309d192bb887b6dbe4afddb8c
Danielepopo <daniballo04@gmail.com>
2026-03-28 23:56:43 +0100

    Fix Bridging Header paths and public headers search

--- ios/Runner/Runner-Bridging-Header.h
+++ ios/Runner/Runner-Bridging-Header.h
@@ -1,4 +1,11 @@
+#ifndef Runner_Bridging_Header_h
+#define Runner_Bridging_Header_h
+
 #import "GeneratedPluginRegistrant.h"
-#import <cloud_firestore/FLTFirebaseFirestorePlugin.h>
-#import <firebase_auth/FLTFirebaseAuthPlugin.h>
-#import <firebase_core/FLTFirebaseCorePlugin.h>
+
+// Usiamo il formato <Plugin/Plugin.h> che è quello standard dei Pods
+#import <cloud_firestore/cloud_firestore-umbrella.h>
+#import <firebase_auth/firebase_auth-umbrella.h>
+#import <firebase_core/firebase_core-umbrella.h>
+
+#endif
