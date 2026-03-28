c6726c86b811486a5f6dad5acab30d4b6c750cbd
Danielepopo <daniballo04@gmail.com>
2026-03-28 21:55:51 +0100

    Reset AppDelegate to standard

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -1,7 +1,5 @@
 import UIKit
 import Flutter
-// Rimuoviamo gli import diretti di Firebase se danno errore e usiamo solo:
-import Firebase
 
 @main
 @objc class AppDelegate: FlutterAppDelegate {
@@ -9,10 +7,7 @@ import Firebase
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
-
-    // Registrazione standard automatica (ora che abbiamo pulito i Pods dovrebbe andare)
     GeneratedPluginRegistrant.register(with: self)
-
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
 }
