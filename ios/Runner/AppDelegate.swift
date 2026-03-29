c287670901b855fc426b6f7224271f7aeac8f5d7
Danielepopo <daniballo04@gmail.com>
2026-03-29 13:27:01 +0200

    Fix: final bypass for GeneratedPluginRegistrant and bump to +12

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -1,13 +1,13 @@
 import UIKit
 import Flutter
 
-@main // Torniamo a @main visto che Xcode lo preferisce, ma puliamo la struttura
+@main
 @objc class AppDelegate: FlutterAppDelegate {
   override func application(
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
-    // Registra i plugin PRIMA di chiamare il super.application
+    // Registrazione dei plugin (Firebase, ecc.)
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
