f4434a36000b2b09588f59cb5c5bcfc12b6b3443
Danielepopo <daniballo04@gmail.com>
2026-03-29 13:04:13 +0200

    Fix: forced ios config generation and updated AppDelegate to @main

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -1,13 +1,13 @@
 import UIKit
 import Flutter
 
-@UIApplicationMain
+@main // Torniamo a @main visto che Xcode lo preferisce, ma puliamo la struttura
 @objc class AppDelegate: FlutterAppDelegate {
   override func application(
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
-    // Registra i plugin di Flutter (Firebase, Firestore, ecc.)
+    // Registra i plugin PRIMA di chiamare il super.application
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
